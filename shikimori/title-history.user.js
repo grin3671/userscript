98af6bda217674c0563aabc76796406d8bede427
grin3671 <grin3671@yandex.ru>
2023-04-15 01:50:42 +0300

    [title-history] increase version

--- shikimori/title-history.user.js
+++ shikimori/title-history.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         История изменений тайтла
 // @namespace    http://tampermonkey.net/
-// @version      1.6
+// @version      1.7
 // @description  Просмотр истории действий с отдельным тайтлом.
 // @author       grin3671
 // @license      MIT
